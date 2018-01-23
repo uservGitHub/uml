6403ae5572c9f5e060973851e01cbb57e6241096
uservGitHub <userv@126.com>
2018-01-23 09:53:31 +0800

    uml工程模板支持即使显示

---
+++ sample/app/src/main/java/com/example/work/umlsample/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.work.umlsample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by work on 2018/1/23.
+ */
+class MainActivity:AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
